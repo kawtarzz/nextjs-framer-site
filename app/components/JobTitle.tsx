6a3dfe454396342ac992def8f3d80ca7d2fcc58c
kawtarzz <kawtaryazzouzi@gmail.com>
2024-01-30 21:08:06 -0600

    update pages

--- app/components/JobTitle.tsx
+++ app/components/JobTitle.tsx
@@ -1,6 +1,4 @@
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useAnimation } from "framer-motion";
 import { useState, useEffect } from "react";
 import "@/app/globals.css";
 
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -3,9 +3,27 @@ import React from "react";
 import "@/app/globals.css";
 
 export default function Resume() {
+  const downloadPDF = () => {
+    window.open(
+      "https://indd.adobe.com/view/9ef0e468-663e-4edd-be5c-5b39592b5f39",
+      "_blank"
+    );
+  };
+
   return (
-    <section className="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
-      <div className="container">Resume</div>
-    </section>
+    <div className="container resume-page">
+      <button className="flat-button" onClick={() => downloadPDF()}>
+        Download Resume
+      </button>
+      <div className="resume-iframe">
+        <iframe
+          style={{ border: " 1px solid #777" }}
+          src="https://indd.adobe.com/view/9ef0e468-663e-4edd-be5c-5b39592b5f39"
+          title="kawtar azzouzi resume"
+          width="800px"
+          height="800px"
+        ></iframe>
+      </div>
+    </div>
   );
 }
